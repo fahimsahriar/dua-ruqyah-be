b58f0365e74634a36b6ad1d4dba78eb7e6af11e4
Fahim Sahriar <fahim.sahriar@sjinnovation.com>
2024-03-19 20:52:06 +0600

    deployment

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const duaRouter = require('./routes/duas.js');
+const cors = require('cors');
 
 const app = express();
-
+// Enable CORS for all routes
+app.use(cors());
 // Define your API endpoints here
+
+// Middleware to dynamically replace hostname in request's base URL
+app.use((req, res, next) => {
+    req.url = req.url.replace(`http://${req.hostname}`, '');
+    next();
+  });
 // Use route modules
+app.use('/', (req, res, next) => {
+    res.send('API is running');
+  });
 app.use('/api/duas', duaRouter);
 
 // Start the server
