e1cafb52cb5c49c5245d58da8fa8277985e1e8c4
Fahim Sahriar <fahim.sahriar@sjinnovation.com>
2024-03-19 21:15:06 +0600

    deployment 8

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-// const express = require('express');
-// const duaRouter = require('./routes/duas.js');
-// const cors = require('cors');
+const express = require('express');
+const duaRouter = require('./routes/duas.js');
+const cors = require('cors');
 
-// const app = express();
-// // Enable CORS for all routes
-// app.use(cors());
-// // Define your API endpoints here
-
-// // Middleware to dynamically replace hostname in request's base URL
-// app.use((req, res, next) => {
-//     req.url = req.url.replace(`http://${req.hostname}`, '');
-//     next();
-//   });
-// // Use route modules
-// app.use('/', (req, res, next) => {
-//     res.send('API is running');
-//   });
-// app.use('/api/duas', duaRouter);
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-const express = require("express");
 const app = express();
+// Enable CORS for all routes
+app.use(cors());
+// Define your API endpoints here
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// Middleware to dynamically replace hostname in request's base URL
+app.use((req, res, next) => {
+    req.url = req.url.replace(`http://${req.hostname}`, '');
+    next();
+  });
+// Use route modules
+app.use('/', (req, res, next) => {
+    res.send('API is running');
+  });
+app.use('/api/duas', duaRouter);
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
