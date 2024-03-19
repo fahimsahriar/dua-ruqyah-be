48d96c90381327d8b42e1a7e53791c3612a7da62
Fahim Sahriar <fahim.sahriar@sjinnovation.com>
2024-03-19 21:04:07 +0600

    deployment 5

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/', (req, res, next) => {
 app.use('/api/duas', duaRouter);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
